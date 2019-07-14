0e5715ab8d45e49c5e9242c7e607d2b97fd91617
Michael Vance <mexvance04@gmail.com>
2019-07-14 15:34:08 -0600

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+            CameraButton2.Clicked += OnTakePhotoButtonClicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() {
+                PhotoSize = PhotoSize.Small
+                //Directory = "Sample",
+                //Name = "test.jpg"
+            });
+
+            //await DisplayAlert("File Location", photo.Path, "OK");
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+        async void OnTakePhotoButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new CameraView());
+        }
+    }
+}
